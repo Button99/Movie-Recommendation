d177e8895cf8a4d456f312e331c43c9f89d66766
Button99 <56029580+Button99@users.noreply.github.com>
2020-07-24 12:59:07 +0300

    GUI Addedv1

--- Application.py
+++ Application.py
@@ -1,15 +1,57 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout, QLineEdit
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLineEdit
+from PyQt5.QtCore import pyqtSlot
 
-app=QApplication([])
 
-window= QWidget()
-layout=QVBoxLayout()
+class App(QWidget):
 
-layout.addWidget(QPushButton("Search"))
-layout.addWidget(QPushButton("Cancel"))
-layout.addWidget(QLineEdit())
+    def __init__(self):
+        super().__init__()
+        self.title = 'Movie Recommendation'
+        self.left = 50
+        self.top = 50
+        self.width = 300
+        self.height = 300
+        self.initUI()
 
-window.setLayout(layout)
-window.show()
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
 
-app.exec_()
+        self.textbox= QLineEdit(self)
+        self.textbox.move(50, 60)
+        self.textbox.resize(170, 30)
+
+        self.button1 = QPushButton("Search Button", self)
+        self.button1.setToolTip("Click to search similar movies")
+        self.button1.move(50, 100)
+        self.button1.clicked.connect(self.click_SearchButton)
+
+        self.button2= QPushButton("Cancel Button", self)
+        self.button2.setToolTip("Click to close the window")
+        self.button2.move(130, 100)
+        self.button2.clicked.connect(self.click_CancelButton)
+
+        text= QLineEdit("Click")
+        #text.move(70, 100)
+
+        self.show()
+
+    @pyqtSlot()
+    def click_SearchButton(self):
+        print("Search button click")
+        txtValue= self.textbox.text()
+        print(txtValue)
+        # File Creation
+        f=open("SimMovie.txt", "w")
+        f.write(txtValue)
+        f.close()
+
+    def click_CancelButton(self):
+        print("Cancel button click")
+        sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
